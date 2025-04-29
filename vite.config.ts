ac262e59b293d7e958a65f408f7bed9297e3dd98
Elziarr <elziarr@gmail.com>
2025-04-29 23:06:21 +0800

    Add deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,22 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     tailwindcss(),
     svelte(),
     Icons({
       compiler: 'svelte',
+    }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`,
     }),
   ],
 });
